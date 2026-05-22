9f671840e9cf60b291897fc0559d45b4e16eab94
Victor K <victor_kuzinov@mail.ru>
2026-05-22 16:22:27 +0500

    Add recipe cleanup and nutrition calculation flow

--- ai/gigachat.py
+++ ai/gigachat.py
@@ -7,7 +7,8 @@ from typing import Optional
 from dotenv import load_dotenv
 from pathlib import Path
 
-from config_ai import PROMPT_GIGACHAT
+from config_ai import PROMPT_GIGACHAT, CONFIG, PROMPT_GIGACHAT_PECIPE
+from services.message_ai_parser import clean_recipe_output
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -218,17 +219,89 @@ def call_gigachat_vision(image_path: str, token: str) -> str | None:
 
     return result["choices"][0]["message"].get("content")
 
+def generate_user_prompt(data: dict) -> str:
+    user_prompt = f"""
+        Ты нутрициолог и повар.
+
+        Составь рецепт блюда.
+
+        Основной запрос:
+        {data["recipe"]}
+
+        Количество человек:
+        {data["persons"]}
+
+        Ограничение по калориям:
+        {data["kcal"]}
+
+        Дополнительные пожелания:
+        {data["wishes"]}
+
+        Требования:
+        - краткий формат;
+        - список ингредиентов;
+        - пошаговое приготовление;
+        - примерная калорийность;
+        - без длинных вступлений.
+    """
+    return user_prompt
+
+
+def call_gigachat_recipe(token: str, prompt: str) -> str | None:
+
+    payload = {
+        "model": "GigaChat-Pro",
+        "messages": [
+            {
+                "role": "system",
+                "content": CONFIG["recipe"]["system_prompt"],
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        "temperature": 0.3,
+        "max_tokens": 400,
+    }
+
+    response = requests.post(
+        "https://gigachat.devices.sberbank.ru/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        },
+        json=payload,
+        verify=False,
+    )
+
+    result = response.json()
+    logger.debug("CHAT:", result)
+
+    if response.status_code != 200:
+        return None
+
+    return result["choices"][0]["message"].get("content")
 
 if __name__ == "__main__":
+    data ={}
 
     access_token = get_access_token()
 
-    image_url = "https://i.ibb.co/whzjRQ6Z/image.jpg"  # плов
+    data["recipe"] = "Хочу приготовить блюдо из курицы"
+    data["persons"] = "на 6 человек"
+    data["kcal"] = "на 1000 килокалорий"
+    data["wishes"] = "Хочу средиземноморскую кухню на обед"
+    user_message = generate_user_prompt(data)
+
+    # image_url = "https://i.ibb.co/whzjRQ6Z/image.jpg"  # плов
     # image_url = "https://i.ibb.co/sJsXgjSh/download.jpg" ## борщ
 
-    image_path =  BASE_DIR / "picture/temp.jpg"
-
-    download_image(image_url, image_path)
-
-    content = call_gigachat_vision(image_path, access_token)
-    logger.info(content)
+    # image_path =  BASE_DIR / "picture/temp.jpg"
+    #
+    # download_image(image_url, image_path)
+    #
+    # content = call_gigachat_vision(image_path, access_token)
+    content = call_gigachat_recipe(access_token, user_message)
+    result = clean_recipe_output(content)
+    logger.info(result)
